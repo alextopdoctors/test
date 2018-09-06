4899fb220d9a6cfdb3aef1dc5a81f10376ca6cf5
alextopdoctors <afernandez@topdoctors.es>
2018-09-06 12:20:06 +0200

    folder path corrected

---
+++ pages/topdoctors/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="title">
+    <h1>
+        Top Farma
+    </h1>
+</div>
+</template>
+
+<style scoped>
+    .title{
+ width: 100%;
+ display: flex;
+ flex-flow: row wrap;
+ justify-content: center;
+
+    }
+</style>
